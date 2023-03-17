cb018ab448ca34fc75b4b983a21051e958b3241f
OlyaGurt <olga.2000.11@gmail.com>
2023-03-17 13:08:57 +0700

    Add gitgnore

--- iosSchool/ColorGenerator.swift
+++ iosSchool/ColorGenerator.swift
@@ -1,17 +1,9 @@
-//
-//  ColorGenerator.swift
-//  iosSchool
-//
-//  Created by Ольга Гуртуева on 13.03.2023.
-//
-
 import Foundation
 
 enum Brightnes: Double {
     case dim = 0.1
     case average = 0.5
     case bright = 1
-    
     func description() -> String {
         switch self {
         case .dim:
@@ -28,27 +20,22 @@ struct Color {
     let red: Double
     let green: Double
     let blue: Double
-    
     var alpha: Brightnes
-    
-    func description(){
-        print("R: \(red), G: \(green), B: \(blue), alpha: \(alpha.rawValue)")
+    func description() {
+    print("R: \(red), G: \(green), B: \(blue), alpha: \(alpha.rawValue)")
     }
 }
 
 class ColorGenerator {
-    let colorCodes: [Double] = [0.0, 51.0, 102.0, 153.0, 204,0, 255.0]
+    let colorCodes: [Double] = [0.0, 51.0, 102.0, 153.0, 204, 0, 255.0]
     var alpha: Double
-    
     init(alpha: Double) {
         self.alpha = alpha
     }
-    
-    init?(alpha: Double?){
+    init?(alpha: Double?) {
         guard let alpha, alpha > 0 && alpha < 1 else {
             return nil
         }
-        
         self.alpha = alpha
     }
 }
--- iosSchool/ViewController.swift
+++ iosSchool/ViewController.swift
@@ -1,24 +1,13 @@
-//
-//  ViewController.swift
-//  iosSchool
-//
-//  Created by Ольга Гуртуева on 13.03.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        var newGen = ColorGenerator(alpha: 1)
+        let newGen = ColorGenerator(alpha: 1)
         newGen.alpha = 0.5
-        var color = Color(red: 1, green: 1, blue: 1, alpha: .bright)
+        let color = Color(red: 1, green: 1, blue: 1, alpha: .bright)
         color.description()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
