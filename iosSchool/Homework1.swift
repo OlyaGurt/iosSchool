85bb9cf4809eeb6018a6b7777098c99a242bda77
OlyaGurt <olga.2000.11@gmail.com>
2023-03-17 14:45:47 +0700

    Add Player

--- iosSchool/Homework1.swift
+++ iosSchool/Homework1.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+let maxHealth = 100
+
 protocol Creature {
     var attack: Int { get set }
     var protection: Int? { get set }
@@ -10,4 +12,11 @@ enum PlayerLevel: Double {
 case low = 0.1
 case medium = 0.25
 case high = 0.5
+}
+
+class Player: Creature {
+    private var counterOfHeal = 3
+    var attack: Int
+    var protection: Int?
+    var health: Int
 }
