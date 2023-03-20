65d2fb9133fa0422e1582e5b240b2459dbede6ae
OlyaGurt <olga.2000.11@gmail.com>
2023-03-20 11:01:51 +0700

    Lesson2

---
+++ iosSchool/Assembly.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+class Assembly {
+
+    func colorGeneratore(alpha: Double) -> ColorGenerator {
+        ColorGenerator(alpha: alpha)
+    }
+}
--- iosSchool/ViewController.swift
+++ iosSchool/ViewController.swift
@@ -8,6 +8,8 @@ class ViewController: UIViewController {
         newGen.alpha = 0.5
         let color = Color(red: 1, green: 1, blue: 1, alpha: .bright)
         color.description()
-        Fight.doFight()
+//        Fight.doFight()
+        let generator: ColorGeneratorProtocol
+        generator = ColorGenerator(alpha: 1)
     }
 }
