0b8dd6f9c32cc16f5ab875f663d611993edd3db3
OlyaGurt <olga.2000.11@gmail.com>
2023-04-06 13:09:13 +0700

    Fix ") {"

--- iosSchool/Responses/ApiClient/RegistrationApiClient.swift
+++ iosSchool/Responses/ApiClient/RegistrationApiClient.swift
@@ -12,7 +12,8 @@ extension ApiClient: RegistrationApiClient {
     func registration(
         username: String,
         password: String,
-        onRequestCompleted: @escaping (Result<TokenResponse, ApiError>) -> Void) {
+        onRequestCompleted: @escaping (Result<TokenResponse, ApiError>) -> Void
+    ) {
         let url = NetworkConstants.URLStrings.nanoPost + "/auth/register"
         let jsonData = try? JSONSerialization.data(withJSONObject: ["username": username, "password": password])
         performRequest(url: url, data: jsonData, method: .post, onRequestCompleted: onRequestCompleted)
